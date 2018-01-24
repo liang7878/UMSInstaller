865dc5da21358e5d56cba20088068a096003922a
chengliangshi <chengliangshi@whu.edu.cn>
2018-01-24 03:16:48 -0800

    save

--- ldap.sh
+++ ldap.sh
@@ -2,3 +2,7 @@
 sudo apt-get update
 sudo apt-get install slapd ldap-utils
 sudo dpkg-reconfigure slapd
+sudo apt-get install phpldapadmin
+
+#file config
+sudo sed -i -e 's/password_hash/password_hash_custom/g' /usr/share/phpldapadmin/lib/TemplateRender.php
